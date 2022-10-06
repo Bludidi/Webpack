a66549ed3fdd5a945f3714dfd6f58ed1c9985339
Bludidi <bludidi5@gmail.com>
2022-10-06 15:35:42 +0200

    testing

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -2,12 +2,13 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
-  // output: {
-  //   filename: 'main.js',
-  //   path: path.resolve(__dirname, 'dist'),
-  //   clean: true,
-  // },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   optimization: {
     runtimeChunk: 'single',
   },
